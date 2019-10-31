629c5e9e6dd45452726118815812bb1012460ba4
automationtrainingcenter <atcsurya@gmail.com>
2019-10-31 12:27:41 +0530

    initial upload with maven notes and selenium dependency

---
+++ src/test/java/com/banking/keyworddriven/AppTest.java
@@ -0,0 +1,13 @@
+package com.banking.keyworddriven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+	}
+}
