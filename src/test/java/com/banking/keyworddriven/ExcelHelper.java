58da2b8158427a8978bff19d168b3aaadca62ded
automationtrainingcenter <atcsurya@gmail.com>
2019-11-01 12:31:49 +0530

    added config file and input keywords file

---
+++ src/test/java/com/banking/keyworddriven/ExcelHelper.java
@@ -0,0 +1,14 @@
+package com.banking.keyworddriven;
+
+public class ExcelHelper {
+	// open excel
+
+	// count the number of rows
+
+	// count the number of columns
+
+	// read data from a cell
+
+	// close excel
+
+}
